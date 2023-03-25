dab2cb7d746f8dd0e35033e97cd4f904430ccf70
Nelson Amaris <amigonelson1@hotmail.com>
2023-03-24 19:57:20 -0500

    useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'Kakaroto',
+        email: 'kakaroto@mail.com',
+    });
+
+    useEffect(() => {
+
+    }, [])
+
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    }
+
+    return (
+        <>
+            <h1>SimpleForm</h1>
+            <hr />
+            <input type='text'
+                name=' username'
+                className='form-control'
+                placeholder='Username'
+                defaultValue={username}
+                onChange={onInputChange}
+            />
+            <input type='email'
+                name=' email'
+                className='form-control mt-2'
+                placeholder='username@mail.com'
+                defaultValue={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
